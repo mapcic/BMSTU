87f7b74d84095f4573545ff127bf0d9b23e33562
mapcic <mapcic@mail.ru>
2017-03-22 02:52:27 +0300

    Change syntx errors in RTD

--- RTD/getJ.m
+++ RTD/getJ.m
@@ -1,43 +1,33 @@
 %% getJ: function description delta, meff, U, Ez
-function [J] = getJ(delta, meff, U, dU, EFermi)
+function J = getJ(delta, meff, U, dU, EFermi)
 	e = 1.6e-19;
 	eVtoJ = e;
 	JtoEv = e^(-1);
 	me = 9.10938356*1e-31;
 	hbar = 1.0551*1e-34;
-	nm = 1e-9;
 	T = 300;
 	k_B = 1.38e-23;
 	kT = T*k_B;
 
-	EFermi = EFermi*eVtoJ;
-
-
 	function val = TDEz(delta, meff, U, Ez)
-		kLeft = abs( sqrt( 2*meff(1)*me*(Ez(j)-U(1)) )/hbar );
-		kRight = abs( sqrt( 2*meff(end)*me*(Ez(j)-U(1)) )/hbar );
+		kLeft = abs( sqrt( 2*meff(1)*me*(Ez(j)-U(1))*eVtoJ )/hbar );
+		kRight = abs( sqrt( 2*meff(end)*me*(Ez(j)-U(1))*eVtoJ )/hbar );
 
 		[waveLeft, ~] = getWaveFunction(delta, meff, U, Ez);
 
-		T = (kRight/kLeft)*(mi(1)/mi(end))*(abs(waveLeft(:,end)).^2)';
-		D = log( 
-				( 1 + exp( (EFermi + U(1) - Ez)/kT ) ) 
-				./ 
-				( 1 + exp( (EFermi + U(end) - Ez)/kT ) ) 
-			);
+		T = (kRight/kLeft)*(meff(1)/meff(end))*(abs(waveLeft(:,end)).^2)';
+		D = log( ( 1 + exp( eVtoJ*(EFermi + U(1) - Ez)/kT ) ) ./ ( 1 + exp( eVtoJ*(EFermi + U(end) - Ez)/kT ) ) );
 
 		val = T.*D;
 	end
 
 
-	k = ((2*meff*me(1)*e*kT)/(4*pi^2*(hbar)^3));
+	k = ((2*meff(1)*me(1)*e*kT)/(4*pi^2*(hbar)^3));
 	J = k*ones(1, length(dU));
 
 	for j = 1:length(dU)
-		dTDEz = @(Ez)TDEz(
-				U - linspace( 0, dU(j),length(U) ),
-				m, delta, Ez
-			);
-		J(j) = J(j)*integral(dTDEz, 0, 1*qe, 'AbsTol', 1e-30);
+		Uj = U - linspace( 0, dU(j),length(U) );
+		dTDEz = @(Ez) TDEz(delta, meff, Uj, Ez);
+		J(j) = J(j)*integral(dTDEz, 0, max(Uj), 'AbsTol', 1e-30);
 	end
 end
--- RTD/getWaveFunction.m
+++ RTD/getWaveFunction.m
@@ -6,28 +6,31 @@ function [waveLeft, waveRigth] = getWaveFunction(delta, meff, U, Ez)
 	hbar = 1.0551*1e-34;
 	nm = 1e-9;
 
-	EzLen = length(E);
+	EzLen = length(Ez);
 	ULen = length(U);
 
 	Ez = Ez*eVtoJ;
 	U = U*eVtoJ;
 	delta = delta*nm;
 
-	waveLeft = zeros(EzLen, n);
-	waveRigth = zeros(EzLen, n);
+	waveLeft = zeros(EzLen, ULen);
+	waveRigth = zeros(EzLen, ULen);
 
 	for j = 1 : EzLen
 		kLeft = sqrt( 2*meff(1)*me*(Ez(j)-U(1)) )/hbar;
 		kRight = sqrt( 2*meff(end)*me*(Ez(j)-U(1)) )/hbar;
 
+		% delta
+
 		d1 = meff(2:ULen)./meff(1:ULen-1);
-		d2 = 2*delta^2*me*meff(2:end-1).*(E(j) - U(2:end-1)) - 2;
+		d2 = 2*delta^2*me*meff(2:end-1).*(Ez(j) - U(2:end-1)) - 2;
+		d2=[1i*kLeft*delta - 1, d2, 1i*kRight*delta - 1];
 		d3 = meff(1:ULen-1)./meff(2:ULen);
 
 		H = diag(d1, -1) + diag(d2) + diag(d3, +1);
 
-		fLeft = [2*1i*kLeft*delta; zeros(lenU-1, 1)];
-		fRight = [zeros(lenU-1, 1); 2*1i*kRight*delta];
+		fLeft = [2*1i*kLeft*delta; zeros(ULen-1, 1)];
+		fRight = [zeros(ULen-1, 1); 2*1i*kRight*delta];
 
 		waveLeft(j, :) = (inv(H)*fLeft)';
 		waveRigth(j, :) = (inv(H)*fRight)';
